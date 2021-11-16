29139cbbddf3ae2e6ca74f75acdf02708fe41530
0xloem <0xloem@gmail.com>
2021-11-16 10:10:55 -0500

    more setup.py notes

--- setup.py
+++ setup.py
@@ -1,2 +1,7 @@
 
-# git+https://github.com/electrumsv/electrumsv.git
+# bitcoinx
+# e-x == 1.16.0
+
+# don't install the vanilla electrumx package; it has dropped support for all coins but bsv.  install e-x instead.
+
+# not presently used: git+https://github.com/electrumsv/electrumsv.git
