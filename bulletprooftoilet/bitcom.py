4f503ec24c30632e161bed2eb87acbf97355b56a
0xloem <0xloem@gmail.com>
2021-11-16 09:59:23 -0500

    downloads example jpg image from bico-media

--- bulletprooftoilet/bitcom.py
+++ bulletprooftoilet/bitcom.py
@@ -1,59 +1,82 @@
 import bitcoinx
 from collections import namedtuple
-from dataclasses import dataclass, fields
+from dataclasses import dataclass, fields, MISSING
+import typing
+
+convert_bytes_to = {
+    str: lambda bytes: bytes.decode(),
+    int: int,
+    bytes: bytes,
+}
 
 class bitcom:
     bitcom = None
     Op = namedtuple('Op', 'output_idx op_idx op item')
-    def __init__(self, tx):
+    @classmethod
+    def from_tx(cls, tx):
         if type(tx) is str:
             tx = bitcoinx.Tx.from_hex(tx)
-        self.tx = tx
-        self.ops = []
-        for output_idx, output in enumerate(self.tx.outputs):
-            for op_idx, (op, item) in enumerate(output.ops_and_items()):
-                self.ops.append(bitcom.Op(output_idx, op_idx, op, item))
-        self.offset = 1
-        if not self.validate():
-            raise TypeError(tx.to_hex())
-        for idx, field in self.fields():
-            setattr(self, field.name, field.type(self.ops[self.offset + idx].item))
-    def __getitem__(self, idx):
-        return self.ops[self.offset + len(self.fields()) + idx].item
-    def validate(self):
-        return self.ops[self.offset].op == 34 and self.__class__.bitcom in (None, self.bitcom)
+        ops = []
+        for output_idx, output in enumerate(tx.outputs):
+            script = output.script_pubkey
+            found_data = False
+            for op_idx, ((op, item), opitem) in enumerate(zip(script.ops_and_items(), script.ops())):
+                if not found_data:
+                    if op == bitcoinx.script.OP_RETURN:
+                        found_data = True
+                    continue
+                ops.append(bitcom.Op(output_idx, op_idx, op, item or opitem))
+        offset = 1
+        bitcom_value = convert_bytes_to[str](ops[0].item)
+        if cls.bitcom not in (None, bitcom_value):
+            raise TypeError(bitcom_value)
+        params = []
+        cls_fields = list(fields(cls))
+        for idx, field in enumerate(cls_fields):
+            if field.default is not MISSING and offset + idx >= len(ops):
+                break
+            if typing.get_origin(field.type) is list and idx + 1 == len(cls_fields):
+                item_type, = typing.get_args(field.type)
+                params.append([convert_bytes_to[item_type](item) for item in ops[offset + idx:]])
+            else:
+                params.append(convert_bytes_to[field.type](ops[offset + idx].item))
+        result = cls(*params)
+        result.bitcom = bitcom_value
+        result.tx = tx
+        return result
 
 @dataclass
 class B(bitcom):
-    bitcom : str = '19HxigV4QyBv3tHpQVcUEQyq1pzZVdoAut'  # https://b.bitdb.network/
+    bitcom = '19HxigV4QyBv3tHpQVcUEQyq1pzZVdoAut'  # https://b.bitdb.network/
     data : bytes
     media_type : str
-    encoding : str
-    filename : str
+    encoding : str = None
+    filename : str = None
 
 @dataclass
 class BCAT(bitcom):
-    bitcom : str = '15DHFxWZJT58f9nhyGnsRBqrgwK4W6h4Up'  # https://bcat.bico.media/
+    bitcom = '15DHFxWZJT58f9nhyGnsRBqrgwK4W6h4Up'  # https://bcat.bico.media/
     info : str
     mime : str
     charset : str
     name : str
     flag : str
+    parts : typing.List[bytes]
 
 @dataclass
 class BCATPART(bitcom):
-    bitcom : str = '1ChDHzdd1H4wSjgGMHyndZm6qxEDGjqpJL'  # https://bcat.bico.media/ (raw data only after prefix)
+    bitcom = '1ChDHzdd1H4wSjgGMHyndZm6qxEDGjqpJL'  # https://bcat.bico.media/ (raw data only after prefix)
     data : bytes
 
 class D(bitcom):
-    bitcom : str = '19iG3WTYSsbyos3uJ733yK4zEioi1FesNU'  # Dynamic - ownership over state of address
+    bitcom = '19iG3WTYSsbyos3uJ733yK4zEioi1FesNU'  # Dynamic - ownership over state of address
     key : str
     value : str
     type : str
     sequence : int
 
 class AIP(bitcom):
-    bitcom : str = '15PciHG22SNLQJXMoSUaWVi7WSqc7hCfva'  # https://github.com/BitcoinFiles/AUTHOR_IDENTITY_PROTOCOL
+    bitcom = '15PciHG22SNLQJXMoSUaWVi7WSqc7hCfva'  # https://github.com/BitcoinFiles/AUTHOR_IDENTITY_PROTOCOL
 
 class MAP(bitcom):
-    bitcom : str = '1PuQa7K62MiKCtssSLKy1kh56WWU7MtUR5'  # MAP protocol.
+    bitcom = '1PuQa7K62MiKCtssSLKy1kh56WWU7MtUR5'  # MAP protocol.
--- test.py
+++ test.py
@@ -1,15 +1,19 @@
 import asyncio
 
-from bulletprooftoilet import electrumx_client, blockchain_module
+from bulletprooftoilet import electrumx_client, blockchain_module, bitcom
 
 blockchainmodule = blockchain_module.BlockchainModule(electrumx_client.ElectrumXClient())
 
 async def main():
     bm0 = await blockchainmodule.submodules()
     bm1 = await bm0[0].submodules();
-    print([txid async for txid in bm1[0].items()])
-    tx = await bm0[0].blockchain.peermanager.tx('a3907e5b910f798c8d0fb450d483a0aefa5ce40ac74064b377603e5ea51deccb')
-    from bulletprooftoilet import bitcom
-    print(bitcom.BCAT(tx))
+    BJPG_TXID = 'a3907e5b910f798c8d0fb450d483a0aefa5ce40ac74064b377603e5ea51deccb'
+    print('block 0 txids:', [txid async for txid in bm1[0].items()])
+    print('downloading a jpeg image from transaction ' + BJPG_TXID)
+    tx = await blockchainmodule.blockchain.peermanager.tx(BJPG_TXID)
+    BJPG = bitcom.B.from_tx(tx)
+    with open(f'{BJPG_TXID}.jpg', 'wb') as jpgout:
+        jpgout.write(BJPG.data)
+        print(f'wrote {BJPG.media_type} to {BJPG_TXID}.jpg')
 
 asyncio.run(main())
