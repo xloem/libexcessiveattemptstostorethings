34b23b00fc2cd5c2436c38ee840d80bcd8903f68
John Doe <johndoe@example.com>
2021-12-11 10:29:10 -0500

    dep ref update

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@
 
 # pycoinnet not used yet
 #    xloem's pr on main repo
-#      git+https://github.com/richardkiss/pycoinnet.git@77f5452f47329d614722a3b6a926f6d9ee0dac7a
+#      latest: git+https://github.com/richardkiss/pycoinnet.git@69d56ee67c840d9f7f5f8807e74a5ac8767c3553
+#      outdated: git+https://github.com/richardkiss/pycoinnet.git@77f5452f47329d614722a3b6a926f6d9ee0dac7a
 #    wip, newer
 #      git+https://github.com/richardkiss/pycoinnet.git@remix
